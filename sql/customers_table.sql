573ea85ad9fe1d91be179157ca2209ac4e376691
aparna32 <113120040+aparna32@users.noreply.github.com>
2025-12-07 03:32:34 +0530

    Create customers_table.sql

---
+++ sql/customers_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(50),
+    region VARCHAR(50)
+);
+
+INSERT INTO customers VALUES
+(1, 'John Doe', 'North'),
+(2, 'Aparna Singh', 'West'),
+(3, 'Maria Lopez', 'South'),
+(4, 'David Kim', 'East');
